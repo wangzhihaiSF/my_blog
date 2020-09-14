2c3f8b77ea5b51aef15fd47759644a8403dc55ae
wangzhihai <wangzhihai@jiezhirong.com>
2020-09-14 16:37:17 +0800

    增加文章详情及详情入口

--- article/urls.py
+++ article/urls.py
@@ -6,4 +6,5 @@ app_name = "article"
 
 urlpatterns = [
     path("article-list", views.article_list, name="article_list"),
+    path("article-detail/<int:article_id>/", views.article_detail, name="article_detail")
 ]
